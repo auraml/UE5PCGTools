6a89b028ca41013767a506b430c08ccb7d575b8d
Arjun Gupta <arjuntheprogrammer@gmail.com>
2024-06-17 11:00:50 +0000

    completed prompt dataset generation

--- Scripts/dataset/prompt_generation/prompt_generator.py
+++ Scripts/dataset/prompt_generation/prompt_generator.py
@@ -21,6 +21,7 @@ def generate_prompt(input_config):
 
 
 if __name__ == "__main__":
+    start_index = 0
     json_file_dir = "D:\\UnrealProjects\\PCGTools\\Scripts\\dataset\\output\\input"
 
     json_files = [os.path.join(json_file_dir, f) for f in os.listdir(
@@ -28,15 +29,17 @@ if __name__ == "__main__":
 
     # sort files by date creaeted
     json_files.sort(key=os.path.getctime, reverse=False)
-    json_files = json_files[100:]
-    print("json_files: ", json_files)
+    json_files = json_files[start_index:]
+    print(len(json_files))
+    # print("json_files: ", json_files)
 
-    output_dir = "D:\\UnrealProjects\\PCGTools\\Scripts\\testing\\output"
+    output_dir = "D:\\UnrealProjects\\PCGTools\\Scripts\\dataset\\prompt_generation\\output"
     for index, file in enumerate(json_files):
         with open(file, "r") as f:
             input_config = f.read()
             response = generate_prompt(input_config)
-            output_file = os.path.join(output_dir, f"{index}.txt")
+            output_file = os.path.join(
+                output_dir, f"{str(start_index+index)}.txt")
             with open(output_file, "w") as f:
                 f.write(response)
                 print(f"Generated prompt for {file}")
